1735679073e3dc56b26a76b6220d43d51aabc2d5
jaccong <151510379+jaccong@users.noreply.github.com>
2025-12-05 11:37:38 +0800

    Update 108.py

--- 108.py
+++ 108.py
@@ -100,8 +100,9 @@ all_links=re.sub(r'\S*无线新闻\S*\,','无线新闻台,',all_links)
 all_links=re.sub(r'\S*千禧经典\S*\,','千禧经典台,',all_links)
 all_links=re.sub(r'\S*美亚电影\S*\,','美亚电影台,',all_links)
 all_links = process_multiline_text(all_links + kx, CHANNEL_ALIAS_MAP)
-total = re.findall(r'.*\,.*:\/\/.*',all_links)
 
+total = re.findall(r'.*\,.*:\/\/.*',all_links)
+total = main(total, custom_order)
 ##print(total)
 count=0
 gd_keywords = ['广东卫视','广东体育','广东珠江','广东新闻','广东影视','广东民生','广东少儿', '嘉佳卡通', '大湾区卫视', '翡翠台','无线新闻']
