f64d212617a9ed6a22744d09fa2a24c8dcb87727
jaccong <151510379+jaccong@users.noreply.github.com>
2025-12-09 23:49:54 +0800

    Update 108.py

--- 108.py
+++ 108.py
@@ -66,6 +66,7 @@ kkxv4 = getlink('https://raw.githubusercontent.com/kakaxi-1/IPTV/refs/heads/main
 rihou = getlink('http://rihou.cc:555/gggg.nzk')
 shulao = getlink('https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/Jsnzkpg/Jsnzkpg1')
 bcitv = getlink('https://877622.xyz/m2t.php?url=https://188766.xyz/itv')
+print(bcitv)
 aptv = getlink('https://877622.xyz/m2t.php?url=https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u')
 
 kx = aptv +kkxv4 + bcitv + rihou + shulao
