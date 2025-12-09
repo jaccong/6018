9571579120e04b46858210298bc3858429398d9c
jaccong <151510379+jaccong@users.noreply.github.com>
2025-12-10 00:05:38 +0800

    Update 108.py

--- 108.py
+++ 108.py
@@ -38,13 +38,13 @@ def getlink(url):
  
   try:
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Cache-Control': 'max-age=0'
+    "Pragma": "no-cache",
+    "Connection": "keep-alive",
+    "Accept": "*/*",
+    "User-Agent": "AptvPlayer/1.3.16",
+    "Accept-Language": "zh-Hans-CN;q=1.0, zh-Hant-HK;q=0.9, en-CN;q=0.8",
+    "Accept-Encoding": "br;q=1.0, gzip;q=0.9, deflate;q=0.8",
+    "Cache-Control": "no-cache, no-store"
     }
     linktext = requests.get(url, headers=headers, timeout=10).content.decode('utf-8') + '\n'
     #if 'Warning' not in linktext and 'Not Found' not in linktext and 'not found' not in linktext:
