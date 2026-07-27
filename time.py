ef9f8fd238a02a2648e9e87706e8065021d8ae5f
jaccong <151510379+jaccong@users.noreply.github.com>
2026-07-27 22:22:21 +0800

    Update time.py

--- time.py
+++ time.py
@@ -2,6 +2,6 @@ import datetime
 
 if __name__ == "__main__":
     now = (datetime.datetime.now() + datetime.timedelta(hours=8)).strftime('[ %m/%d %H:%M ]')
-    with open("test.txt", 'w', encoding='utf-8') as file:
+    with open("time.txt", 'w', encoding='utf-8') as file:
         file.write("自动更新,#genre#\n")
         file.write(f"{now},https://jaccong0520.serv00.net/da.mp4\n")
