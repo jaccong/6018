f30c473a6c955380dbd1b15f1ca57f04689356e1
jaccong <151510379+jaccong@users.noreply.github.com>
2026-08-05 09:43:21 +0800

    Update 108.py

--- 108.py
+++ 108.py
@@ -11,7 +11,7 @@ TIMEOUT = 10                   # 网络请求超时时间
 # 格式：[(资源标识, URL/None), ...] | URL=None→本地文件，列表顺序=合并顺序
 RESOURCE_MERGE_CONFIG = [
     ('cf_live', 'https://o.877622.xyz/live'),
-    ('mengmeng','http://45.192.97.170:5211/365987')
+    ('kulao','http://877622.xyz/2t.php?url=https://live.445569.xyz/live.m3u')
 ]
 
 # 3. 频道分类配置
