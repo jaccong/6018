ca5df68d98cabf8aba139e698826eee8e2885f7a
jaccong <151510379+jaccong@users.noreply.github.com>
2026-08-12 10:58:50 +0800

    Update 108.py

--- 108.py
+++ 108.py
@@ -10,10 +10,10 @@ TIMEOUT = 10                   # 网络请求超时时间
 # 2. 资源合并配置（参数区合并：资源信息+合并顺序绑定）
 # 格式：[(资源标识, URL/None), ...] | URL=None→本地文件，列表顺序=合并顺序
 RESOURCE_MERGE_CONFIG = [
-    ('http://877622.xyz/gdty.txt'),
+    ('gdty','http://877622.xyz/gdty.txt'),
     ('cf_live', 'https://o.877622.xyz/live'),
     ('kulao','http://877622.xyz/2t.php?url=https://live.445569.xyz/live.m3u'),
-    ('https://877622.xyz/2t.php?url=https://m3u.877622.xyz/xymm')
+    ('cflive2','https://877622.xyz/2t.php?url=https://m3u.877622.xyz/xymm')
 ]
 
 # 3. 频道分类配置
